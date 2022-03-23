be25cc50fe1b847fcc2b167e2b9b05b496279709
KHAGESHOR GIRI <asndpmax@outlook.com>
2022-03-23 17:32:14 +0545

    initial Project Setup

---
+++ Library_MS/Model_DBContext/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Library_MS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_MS.Model_DBContext
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Library_MS/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_MS.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+    }
+}
